18f1bfb2c770d34d749494342701f1e1544986be
pumatus <1009441951@qq.com>
2017-01-22 12:34:26 +0800

    add

--- app/src/main/java/com/photo/vision/visionphoto/OCRActivity.java
+++ app/src/main/java/com/photo/vision/visionphoto/OCRActivity.java
@@ -1,8 +1,23 @@
 package com.photo.vision.visionphoto;
 
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.vision.CameraSource;
+
 /**
  * Created by Hongd on 2017/1/21.
  */
 
-public class OCRActivity {
+public class OCRActivity extends AppCompatActivity{
+    private static final int RC_HANDLE_GMS = 9001;
+
+    private static final String TAG = "OcrCaptureActivity";
+
+    private static final int RC_HANDLE_VAMERA_PERM = 2;
+
+    public static final String AutoFocus = "AutoFocus";
+    public static final String UseFlash  = "UseFlash";
+
+    private CameraSource mCameraSource;
+//    private CameraSourcePreView mCameraSourcePreView;
 }
--- app/src/main/java/com/photo/vision/visionphoto/ocr/CameraSource.java
+++ app/src/main/java/com/photo/vision/visionphoto/ocr/CameraSource.java
@@ -1,8 +1,15 @@
 package com.photo.vision.visionphoto.ocr;
 
+import android.annotation.SuppressLint;
+import android.hardware.Camera;
+
 /**
  * Created by Hongd on 2017/1/22.
  */
 
 public class CameraSource {
+    @SuppressLint("InlinedApi")
+    public static final int CAMERA_FACING_BACK = Camera.CameraInfo.CAMERA_FACING_BACK;
+    @SuppressLint("InlinedApi")
+    public static final int CAMERA_FACING_FRONT = Camera.CameraInfo.CAMERA_FACING_FRONT;
 }
