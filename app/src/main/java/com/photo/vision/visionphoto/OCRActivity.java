1858b7cca8e1d7875839c9aa654aecfc178fbde3
pumatus <1009441951@qq.com>
2017-01-26 09:09:50 +0800

    add

--- app/src/main/java/com/photo/vision/visionphoto/OCRActivity.java
+++ app/src/main/java/com/photo/vision/visionphoto/OCRActivity.java
@@ -1,5 +1,7 @@
 package com.photo.vision.visionphoto;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -28,4 +30,10 @@ public class OCRActivity extends AppCompatActivity{
 
     @BindView(R.id.tvOcrResult)
     TextView tvOcrResult;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr);
+    }
 }
